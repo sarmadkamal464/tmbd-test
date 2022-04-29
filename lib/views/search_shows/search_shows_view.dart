a7c80df5446c88a5b6d6feca9b1a8c7886ea6854
sarmad <sarmadkamal464@gmail.com>
2022-04-29 16:12:08 +0500

    upload screenshots

--- lib/views/search_shows/search_shows_view.dart
+++ lib/views/search_shows/search_shows_view.dart
@@ -4,20 +4,21 @@ import 'package:stacked/stacked.dart';
 import '../../widgets/dumb_widgets/appbar.dart';
 import '../../widgets/smart_widgets/appbar_bottomsheet.dart';
 import 'search_shows_view_model.dart';
-          
+
 class SearchShowsView extends StatelessWidget {
   const SearchShowsView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-     var size = MediaQuery.of(context).size;
+    var size = MediaQuery.of(context).size;
     return ViewModelBuilder<SearchShowsViewModel>.reactive(
-      builder: (BuildContext context, SearchShowsViewModel viewModel, Widget _) {
+      builder:
+          (BuildContext context, SearchShowsViewModel viewModel, Widget _) {
         return Scaffold(
           appBar: PreferredSize(
             preferredSize: const Size.fromHeight(130),
             child: AppBarWidget(
-              title: 'Popular TV Shows',
+              title: 'Search TV Shows',
               bottomChild: appbarBottomSheet(size, viewModel, context),
             ),
           ),
